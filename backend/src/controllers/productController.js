43308370f8fc72aaf3c3e80d0fc1f47ebb295a29
Man0ranjitham <manoranjithamkavitha@gmail.com>
2025-09-12 16:55:15 +0530

    Modified productController.js file

---
+++ backend/src/controllers/productController.js
@@ -0,0 +1,20 @@
+const Product = require('../models/Product');
+
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find().select('-__v');
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.createProduct = async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    const saved = await newProduct.save();
+    res.status(201).json(saved);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
